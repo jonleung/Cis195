e5f981df05de48228b4bb6ea8377789b77731e9f
Jonathan Leung <naruto137@gmail.com>
2011-08-02 00:56:56 -0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iphone::Application.config.secret_token = 'a8566ff7e26ba3402abb0486a99791de37fd4ee3dbe1acaf12c39b836dc70dc01fc5b5e690eefd867c6035ae747d9ed4f801be2087b1bbb4dc3948b5bdd1014f'
